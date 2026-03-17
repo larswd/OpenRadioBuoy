c55f3792a63c205ca8ba22691949f1e19a117a34
larswd <larswd@proton.me>
2026-03-17 13:15:35 +0100

    Added thermometre rading example.

---
+++ firmware/examples/read_temperatures/src/config.h
@@ -0,0 +1,50 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include "Arduino.h"
+
+static constexpr uint8_t BUOY_MODE {0};
+static constexpr uint8_t BST_MODE {1};
+
+static constexpr uint8_t WIO_MODE {BUOY_MODE}; 
+
+// Useful constants
+static constexpr uint32_t s_2_ms                         {1000};
+static constexpr uint32_t min_2_s                          {60};
+static constexpr float    mps_2_kmph                       {3.6};
+
+
+// Sensor reading parameters
+static constexpr uint8_t  measurements_per_packet            {15};
+static constexpr uint8_t  max_number_of_measurements         {80};
+static constexpr uint32_t max_GPS_read_time                  {3*min_2_s*s_2_ms};
+static constexpr uint32_t max_sensor_read_time               {40*s_2_ms};
+static constexpr float    outlier_discard_tolerance          {2};
+static constexpr uint16_t GPS_baud                           {9600};
+static constexpr uint32_t minimal_measurement_period         {10*s_2_ms};
+static           uint32_t base_measurement_period            {5*min_2_s*s_2_ms};
+static constexpr uint32_t maximal_measurement_period         {30*min_2_s*s_2_ms};
+static constexpr bool     resync_RTC_using_GPS               {true};
+static constexpr uint32_t scale_factor                       {100000};
+
+// Pin config. Do not change unless you have rewired the OLB
+static constexpr uint8_t  GPS_RX_PIN                     {PC0};
+static constexpr uint8_t  GPS_TX_PIN                     {PC1};
+static constexpr uint8_t  GPS_SLEEP_PIN                  {PA0};
+
+// Enable or disable parameters
+static constexpr bool remove_outliers           {true};
+static constexpr bool debug_serial              {true};
+static constexpr bool enable_GPS                {true};
+static constexpr bool enable_watchdog           {true};
+static constexpr int  serial_baud               {115200};
+static           bool enable_motion_detection   {false};
+static constexpr bool sleep_GPS                 {true};
+static constexpr bool debug_SD                  {false};
+// Motion parameters
+static float    motion_treshold                          {0.5};
+static uint32_t targetReadingDistance                    {30};
+
+// Watchdog and power parameters
+static constexpr uint32_t watchdog_wait_time              {32000};
+static constexpr uint32_t sleep_time                      {12000};
+#endif
---
+++ firmware/examples/read_temperatures/src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include "config.h"
+#include "sd_writer.h"
+#include "thermo_manager.h"
+/*
+  This sketch showcase  all the functionality of the GPS library
+
+*/
+
+static uint32_t measurement_timer;
+static constexpr uint8_t thermistorDataPin  {PA9};
+static constexpr uint8_t thermistorPowerPin {PB10};
+
+void setup() {
+  
+  // We start serial output at 115200 baudrate
+  Serial.begin(serial_baud);
+  Serial.println("Serial initialized!");
+  delay(100);
+
+  // We try to enable rtc timing
+  setSyncInterval(1);
+
+
+  // Some thermistor functionality interface with the SD writer
+  // So we have to initialize the SD card writer as well
+  bool SD_fail = sd_writer.begin();
+
+  // We initialize the thermometres at the desired pins
+  thermo_manager.begin(thermistorDataPin, thermistorPowerPin);
+  measurement_timer = millis();
+}
+
+void loop() {
+  // Measurement loop. 
+  if (millis() - measurement_timer > base_measurement_period){
+    
+    thermo_manager.wake();
+
+    //The SD library counts the amount of measurements taken
+    char logname[25];
+    sprintf(logname, "readings/reading%05d.txt", sd_writer.logCount);
+
+    sd_writer.startLogging(logname);
+
+    // The last argument specifies if we want to log each thermistor reading or
+    // Just the filtered end value
+    thermo_manager.takeReadings(max_sensor_read_time, millis(), true);
+    thermo_manager.processReadings();
+    
+    // We should turn off the thermistors to save power
+    thermo_manager.sleep();
+    // Update clock variables
+    measurement_timer = millis();
+    sd_writer.closeLog();
+  }
+  delay(150);
+}
+
